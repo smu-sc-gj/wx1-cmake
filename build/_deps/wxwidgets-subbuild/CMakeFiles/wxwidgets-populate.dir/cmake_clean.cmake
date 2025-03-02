befa3ef071622f88fbb70acdabc3a71db0289e83
Glenn (Old Laptop) Jenkins <gljenkins@cardiffmet.ac.uk>
2025-03-02 21:58:12 +0000

    Decided to see if I could turn this into a CMake project while keeping pauls original structure.

---
+++ build/_deps/wxwidgets-subbuild/CMakeFiles/wxwidgets-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wxwidgets-populate"
+  "CMakeFiles/wxwidgets-populate-complete"
+  "wxwidgets-populate-prefix/src/wxwidgets-populate-stamp/wxwidgets-populate-build"
+  "wxwidgets-populate-prefix/src/wxwidgets-populate-stamp/wxwidgets-populate-configure"
+  "wxwidgets-populate-prefix/src/wxwidgets-populate-stamp/wxwidgets-populate-download"
+  "wxwidgets-populate-prefix/src/wxwidgets-populate-stamp/wxwidgets-populate-install"
+  "wxwidgets-populate-prefix/src/wxwidgets-populate-stamp/wxwidgets-populate-mkdir"
+  "wxwidgets-populate-prefix/src/wxwidgets-populate-stamp/wxwidgets-populate-patch"
+  "wxwidgets-populate-prefix/src/wxwidgets-populate-stamp/wxwidgets-populate-test"
+  "wxwidgets-populate-prefix/src/wxwidgets-populate-stamp/wxwidgets-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/wxwidgets-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/_deps/wxwidgets-subbuild/CMakeFiles/wxwidgets-populate.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for wxwidgets-populate.
+# This may be replaced when dependencies are built.
